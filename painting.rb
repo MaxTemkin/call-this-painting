2ffc86f5c4698a106ae772d603cc0268eaec141f
Max Temkin <Max@Temkin.com>
2015-04-20 11:46:27 -0500

    removed the r.say

--- painting.rb
+++ painting.rb
@@ -4,7 +4,6 @@ require 'twilio-ruby'
 get '/painting/:name' do
 	#Figure out which painting is being called
 	response = Twilio::TwiML::Response.new do |r|
-		r.Say "Thank you for calling a painting in the Cards Against Humanity gallery."
 	    r.Play "https://s3.amazonaws.com/call-this-painting/#{params[:name]}.mp3"
 	end
 
